5ef2973a9c8813ea48361c59727dc1ae6c66ebb5
Rcaicedo05 <santiago12341233@gmail.com>
2026-06-07 09:37:00 -0500

    Update run.py

--- run.py
+++ run.py
@@ -2,10 +2,8 @@ from app import create_app, db
 
 app = create_app()
 
-# Crea la base de datos local automáticamente al arrancar
 with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
-    
+    app.run(host='0.0.0.0', port=8080, debug=False)
