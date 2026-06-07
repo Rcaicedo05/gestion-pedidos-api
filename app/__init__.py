de9699f0ddf22e965ab30a3567cda2bd68caeb2d
Rcaicedo05 <santiago12341233@gmail.com>
2026-06-07 11:28:16 -0500

    Refactor create_app function and update health route

--- app/__init__.py
+++ app/__init__.py
@@ -8,19 +8,17 @@ ma = Marshmallow()
 def create_app():
     app = Flask(__name__)
 
-    # Configuración base
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/orders.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
     ma.init_app(app)
 
-    # Registro de las rutas (Blueprints)
     from app.routes import orders_bp
     app.register_blueprint(orders_bp, url_prefix='/api')
-    
-@app.route('/health')
+
+    @app.route('/health')
     def health():
         return {'status': 'ok'}, 200
-        
+
     return app
