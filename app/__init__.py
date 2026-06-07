f9f3d3ea673522e7010c9746d810fe68666e8a16
Rcaicedo05 <santiago12341233@gmail.com>
2026-06-07 11:48:25 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,16 @@
-db.init_app(app)
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
+db = SQLAlchemy()
+ma = Marshmallow()
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/orders.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
     ma.init_app(app)
 
     with app.app_context():
